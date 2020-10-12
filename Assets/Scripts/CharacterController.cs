6d13e317ef1cd912bab77b6a1804ee195439f164
Leandro Rodrigues <lbrguitar@gmail.com>
2020-10-12 16:46:10 -0400

    Repository fix attempt

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    Vector2 movement;
+    public float moveSpeed = 1.0f;
+    public bool isMovementDisabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            DisableMovement();
+        }
+        //Split movement into two steps, in case we want to move the chacter using somehting else, but here we get input accross the axis,
+        //and then used moved character to move it around. 
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        if (isMovementDisabled)
+        {
+            return;
+        }
+        //Translates the character. 
+        this.gameObject.transform.Translate(new Vector3(movement.x, movement.y, 0.0f) * moveSpeed * Time.deltaTime);
+
+
+    }
+
+    public void DisableMovement()
+    {
+        isMovementDisabled = !isMovementDisabled;
+    }
+
+
+}
---
+++ Assets/Scripts/SimpleInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    string PLAYER_TAG = "Player";
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == PLAYER_TAG)
+        {
+            other.gameObject.GetComponent<CharacterController>().DisableMovement();
+        }
+    }
+}
