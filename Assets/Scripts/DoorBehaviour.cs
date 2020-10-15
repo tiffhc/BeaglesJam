2bd3114580f53110bad7ba46b6855ed97302c68d
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-10-15 15:32:56 +0100

    pushing the first bits of the whitebox.
    added outside and security office.

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    string PLAYER_TAG = "Player";
+    [SerializeField] GameObject cameraLocation;
+    [SerializeField] GameObject playerLocation;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit a thing with door");
+        if (other.gameObject.tag == PLAYER_TAG)
+        {
+            Debug.Log("Went there");
+            Camera.main.transform.position = cameraLocation.transform.position;
+            other.gameObject.transform.position = playerLocation.transform.position;
+        }
+
+    }
+}
