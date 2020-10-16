43be4dadfa8718a7c11e43056777c503f39edaab
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-10-16 20:17:05 +0100

    added locked doors.

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -7,9 +7,14 @@ public class DoorBehaviour : MonoBehaviour
     string PLAYER_TAG = "Player";
     [SerializeField] GameObject cameraLocation;
     [SerializeField] GameObject playerLocation;
+    public bool isDoorUnlocked = true;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        if(!isDoorUnlocked)
+        {
+            return;
+        }
         Debug.Log("Hit a thing with door");
         if (other.gameObject.tag == PLAYER_TAG)
         {
