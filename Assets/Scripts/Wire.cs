a60ff3fe9f70f0de06f582f8ace26b4428b1021b
Leandro Rodrigues <lbrguitar@gmail.com>
2020-10-18 03:02:25 -0400

    Wire frigging finally works

---
+++ Assets/Scripts/Wire.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    // Wire control
+    [SerializeField] private Wire wireEnd = null;
+    [SerializeField] private bool wireStart = false;
+    [SerializeField] private int wireNumber = -1; // To check if it's the correct ending
+
+    // Line control
+    private LineRenderer line;
+    private bool done = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        if(line != null)
+        {
+            Vector3 startPos = new Vector3(transform.position.x, transform.position.y, 0);
+            line.SetPosition(0, startPos);
+            line.SetPosition(1, startPos);
+        }
+    }
+
+    // Function to compute ending of wire (called by mousebuttonUp)
+        // If not at a proper ending, reset wire
+    public void EndWire(bool correct)
+    {
+        if(correct)
+        {
+            Vector3 position = new Vector3(wireEnd.transform.position.x, wireEnd.transform.position.y, 0);
+            line.SetPosition(1, position);
+            done = true;
+        }
+        else
+        {
+            Vector3 position = new Vector3(transform.position.x, transform.position.y, 0);
+            line.SetPosition(1, position);
+        }
+    }
+
+    // Function to update line while having it hold
+        // Update end point
+    public void UpdateWire()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        line.SetPosition(1, new Vector3(mouse.x, mouse.y, 0));
+    }
+
+    public bool IsWireStart()
+    {
+        return wireStart;
+    }
+
+    public int GetWireNumber()
+    {
+        return wireNumber;
+    }
+
+    public bool IsFinished()
+    {
+        return done;
+    }
+}
---
+++ Assets/Scripts/WireMinigameHandler.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WireMinigameHandler : MonoBehaviour
+{
+    private Wire holdingWire = null;
+    private Wire endWireFound = null;
+    private bool isHoldingWire = false;
+    private bool isPlaying = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            // Call for wire update if already dragging it
+            if (isHoldingWire)
+            {
+                holdingWire.UpdateWire();
+            }
+
+            // Get mouse input and check if it's start
+            if (Input.GetMouseButtonDown(0))
+            {
+                CheckWirePickup();
+            }
+
+            // Get mouse button up and if it's holding the wire, call the proper function
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (isHoldingWire)
+                {
+                    CheckWireRelease();
+                }
+            }
+        }
+    }
+
+    private void CheckWirePickup()
+    {
+        // Cast for wire
+        // Cast a ray from camera through the mouse position onto the scene
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            holdingWire = hit.collider.gameObject.GetComponent<Wire>();
+            if (holdingWire != null)
+            {
+                if (holdingWire.IsWireStart() && !holdingWire.IsFinished())
+                {
+                    isHoldingWire = true;
+                }
+            }
+        }
+    }
+
+    private void CheckWireRelease()
+    {
+        // Cast for wire end
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            endWireFound = hit.collider.gameObject.GetComponent<Wire>();
+            if (endWireFound != null && isHoldingWire)
+            {
+                // TODO: maybe just put this entire check in the function call
+                if (!(endWireFound.IsWireStart()) &&
+                        (endWireFound.GetWireNumber() == holdingWire.GetWireNumber()))
+                {
+                    holdingWire.EndWire(true);
+                }
+                else
+                {
+                    holdingWire.EndWire(false);
+                }
+
+                isHoldingWire = false;
+                // TODO: change this to a return to the proper game after minigame is complete inside the if
+                isPlaying = false;
+            }
+        }
+        else
+        {
+            holdingWire.EndWire(false);
+        }
+
+        isHoldingWire = false;
+        // TODO: change this to a return to the proper game after minigame is complete inside the if
+        isPlaying = false;
+    }
+
+    public void StartPlaying()
+    {
+        isPlaying = true;
+    }
+}
