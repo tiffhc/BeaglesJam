d00370decc078b6dcef775fe00b3d12229ef04cf
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-10-18 21:52:13 +0100

    push to fix ami. she now has legs to walk to thellibrary

---
+++ Assets/Scripts/TalkTo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkTo : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] thingstoSpawn;
+    string PLAYER_TAG = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == PLAYER_TAG)
+        {
+            foreach(GameObject thingtoSpawn in thingstoSpawn)
+            {
+                thingtoSpawn.SetActive(true);
+                
+            }
+            this.gameObject.SetActive(false);
+        }
+    }
+}
