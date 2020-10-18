fa2697b6a626e783e60576eb2a7ff0164cae29d1
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2020-10-18 10:13:07 -0400

    Pushed notification box.

---
+++ Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationManager : MonoBehaviour
+{
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    [FMODUnity.EventRef]
+    public string nextDialogueSound = "";
+    [FMODUnity.EventRef]
+    public string endDialogueSound = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Notification dialogue)
+    {
+        animator.SetBool("isOpen", true);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        FMODUnity.RuntimeManager.PlayOneShot(nextDialogueSound);
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(endDialogueSound);
+        animator.SetBool("isOpen", false);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<CharacterController>().DisableMovement();
+    }
+
+    public void SkipDialogue()
+    {
+        sentences.Clear();
+        EndDialogue();
+    }
+}
---
+++ Assets/Scripts/NotificationTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationTrigger : MonoBehaviour
+{
+    string PLAYER_TAG = "Player";
+    public Notification dialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == PLAYER_TAG)
+        {
+
+            other.gameObject.GetComponent<CharacterController>().DisableMovement();
+            TriggerNotification();
+        }
+    }
+    public void TriggerNotification()
+    {
+        FindObjectOfType<NotificationManager>().StartDialogue(dialogue);
+    }
+}
