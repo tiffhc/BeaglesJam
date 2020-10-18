6a688d8f72de94cb9a27b57a4fc9c8d0ff8b0601
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2020-10-18 00:33:02 -0400

    Added Dialogue box UI & mechanism

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    string PLAYER_TAG = "Player";
+    public Dialogue dialogue;
+
+    private void OnTriggerEnter(Collider other) { 
+        if (other.gameObject.tag == PLAYER_TAG)
+        {
+            TriggerDialogue();
+        }
+    }
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
